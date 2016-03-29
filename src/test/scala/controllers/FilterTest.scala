2566ef190fac1456672e1bf2b40eb02a0238b15f
sofa13 <s-lepage@live.ca>
2016-03-29 17:09:15 -0400

    Add new FilterTest class with 5 cases

---
+++ src/test/scala/controllers/FilterTest.scala
@@ -0,0 +1,48 @@
+package controllers
+
+import org.scalatest._
+import steno.{StenoDictionary, DictionaryFormat, DictionaryEntry}
+import scala.io.Source
+
+/**
+  * Created by Sophie on 26/03/2016.
+  */
+class FilterTest extends FlatSpec with Matchers{
+
+  /* Note:
+  * filter(true) means keep entry
+  * filter(false) means hide entry
+  */
+
+  "The filter function" should "keep entries if all queries are empty" in {
+    var entry = new DictionaryEntry("{\\*\\cxs KOPB/SREPBGS}convention", DictionaryFormat.RTF, "main.rtf")
+    var filter = DictionaryEntry.filterDictionaryEntry(null, null, null, null, null)
+    filter(entry) should equal(true)
+    filter = DictionaryEntry.filterDictionaryEntry("", "", "", "", "")
+    filter(entry) should equal(true)
+  }
+  "The filter function" should "be case insensitive" in {
+    var entry = new DictionaryEntry("{\\*\\cxs KOPB/SREPBGS}convention", DictionaryFormat.RTF, "main.rtf")
+    var filter = DictionaryEntry.filterDictionaryEntry("CONVEN", "kopb", null, null, null)
+    filter(entry) should equal(true)
+  }
+  "The filter function" should "keep entries that contain the query" in {
+    var entry = new DictionaryEntry("{\\*\\cxs KOPB/SREPBGS}convention", DictionaryFormat.RTF, "main.rtf")
+    var filter = DictionaryEntry.filterDictionaryEntry("tion", "SRE", "main.rtf", "2", "1")
+    filter(entry) should equal(true)
+  }
+  "The filter function" should "hide entries that do not contain the query" in {
+    var entry = new DictionaryEntry("{\\*\\cxs KOPB/SREPBGS}convention", DictionaryFormat.RTF, "main.rtf")
+    var filter = DictionaryEntry.filterDictionaryEntry("tion", "SRE", "main.rtf", "2", "2")
+    filter(entry) should equal(false)
+    filter = DictionaryEntry.filterDictionaryEntry(" ", null, null, null, null)
+    filter(entry) should equal(false)
+  }
+  "The filter function" should "keep entries if query does not make sense" in {
+    var entry = new DictionaryEntry("{\\*\\cxs KOPB/SREPBGS}convention", DictionaryFormat.RTF, "main.rtf")
+    var filter = DictionaryEntry.filterDictionaryEntry(null, null, "main.rtf", "number", null)
+    filter(entry) should equal(true)
+    filter = DictionaryEntry.filterDictionaryEntry(null, null, "main.rtf", null, "number")
+    filter(entry) should equal(true)
+  }
+}
