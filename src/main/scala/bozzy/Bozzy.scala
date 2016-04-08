4d94e59795d6d255039bea8756e4ea3cfaa7d245
Ted Morin <theodore.morin@clearwaterclinical.com>
2016-04-08 14:06:00 -0400

    Add more reliable emoji font selection, add fixed-width strokes

--- src/main/scala/bozzy/Bozzy.scala
+++ src/main/scala/bozzy/Bozzy.scala
@@ -6,12 +6,18 @@ import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
+import scalafx.scene.text.Font
 import scalafxml.core.{NoDependencyResolver, FXMLView}
 
 /**
   * Created by ted on 2016-04-06.
   */
 object Bozzy extends JFXApp {
+  Font.loadFont(
+    getClass.getResource("/fonts/symbola_hint_8.ttf").toExternalForm,
+    14
+  )
+
   val resource = getClass.getResource("/view/Main.fxml")
   if (resource == null) {
     throw new FileNotFoundException("Cannot load resource: /view/Main.fxml")
